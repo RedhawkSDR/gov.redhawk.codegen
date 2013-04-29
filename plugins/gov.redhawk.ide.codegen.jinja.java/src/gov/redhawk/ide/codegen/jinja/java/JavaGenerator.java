45dfde09e17f93a6ed8bdcff0eff66244f9d4d15
Justin A. Irwin <jirwin@geontech.com>
2013-04-29 12:46:05 -0400

    Configure Java projects to have the requisite paths set up.

--- plugins/gov.redhawk.ide.codegen.jinja.java/src/gov/redhawk/ide/codegen/jinja/java/JavaGenerator.java
+++ plugins/gov.redhawk.ide.codegen.jinja.java/src/gov/redhawk/ide/codegen/jinja/java/JavaGenerator.java
@@ -11,11 +11,19 @@ import mil.jpeojtrs.sca.spd.SpdFactory;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.jdt.core.IJavaProject;
 
 import gov.redhawk.ide.codegen.ImplementationSettings;
+import gov.redhawk.ide.codegen.java.JavaGeneratorPlugin;
+import gov.redhawk.ide.codegen.java.JavaGeneratorUtils;
 import gov.redhawk.ide.codegen.jinja.JinjaGenerator;
 
 public class JavaGenerator extends JinjaGenerator {
@@ -62,8 +70,36 @@ public class JavaGenerator extends JinjaGenerator {
 		return null;
 	}
 
-	public IStatus validate() {
-		return new Status(IStatus.OK, JavaGeneratorPlugin.PLUGIN_ID, "Validation ok");
-	}
+	@Override
+	protected IStatus configureProject(IProject project, ImplementationSettings implSettings, IProgressMonitor monitor) {
+		final int CONFIGURE_STEPS = 3;
+		final int DEFAULT_WORK = 1;
+		final MultiStatus status = new MultiStatus(JavaGeneratorPlugin.PLUGIN_ID, IStatus.OK, "Java code generation problems", null);
+		SubMonitor progress = SubMonitor.convert(monitor, CONFIGURE_STEPS);
+		IJavaProject javaProject = null;
+		try {
+			javaProject = JavaGeneratorUtils.addJavaProjectNature(project, progress.newChild(DEFAULT_WORK));
+		} catch (final CoreException e) {
+			status.add(new Status(IStatus.ERROR, JavaGeneratorPlugin.PLUGIN_ID, "Unable to add Java project nature", e));
+			return status;
+		}
 
+		final IPath destinationBinDirectory = new Path(implSettings.getOutputDir()).append("bin");
+		final IPath destinationSrcDirectory = new Path(implSettings.getOutputDir()).append("src");
+		try {
+			final IPath srcPath = new Path(javaProject.getPath().toString() + "/" + destinationSrcDirectory);
+			final IPath binPath = new Path(javaProject.getPath().toString() + "/" + destinationBinDirectory);
+			JavaGeneratorUtils.addSourceClassPaths(javaProject, srcPath, binPath, progress.newChild(DEFAULT_WORK));
+		} catch (final CoreException e) {
+			status.add(new Status(IStatus.WARNING, JavaGeneratorPlugin.PLUGIN_ID, "Unable to add Java source path", e));
+		}
+
+		try {
+			JavaGeneratorUtils.addRedhawkJavaClassPaths(javaProject, progress.newChild(DEFAULT_WORK));
+		} catch (final CoreException e) {
+			status.add(new Status(IStatus.WARNING, JavaGeneratorPlugin.PLUGIN_ID, "Unable to add setup Java class paths", e));
+		}
+
+		return status;
+	}
 }
