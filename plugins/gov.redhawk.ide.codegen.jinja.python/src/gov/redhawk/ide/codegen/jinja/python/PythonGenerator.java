1e744d094de2a0c4623e6deca5c968f16b721a4c
Justin A. Irwin <jirwin@geontech.com>
2013-04-29 10:09:06 -0400

    Remove unused imports and function already defined in base class.

--- plugins/gov.redhawk.ide.codegen.jinja.python/src/gov/redhawk/ide/codegen/jinja/python/PythonGenerator.java
+++ plugins/gov.redhawk.ide.codegen.jinja.python/src/gov/redhawk/ide/codegen/jinja/python/PythonGenerator.java
@@ -1,12 +1,6 @@
 package gov.redhawk.ide.codegen.jinja.python;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 import mil.jpeojtrs.sca.spd.Code;
 import mil.jpeojtrs.sca.spd.CodeFileType;
@@ -18,8 +12,6 @@ import mil.jpeojtrs.sca.spd.SpdFactory;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
@@ -27,9 +19,7 @@ import org.eclipse.core.runtime.Status;
 import org.python.pydev.core.IInterpreterManager;
 import org.python.pydev.plugin.PydevPlugin;
 
-import gov.redhawk.ide.codegen.FileToCRCMap;
 import gov.redhawk.ide.codegen.ImplementationSettings;
-import gov.redhawk.ide.codegen.Property;
 import gov.redhawk.model.sca.util.ModelUtil;
 import gov.redhawk.ide.codegen.jinja.JinjaGenerator;
 
@@ -39,28 +29,6 @@ public class PythonGenerator extends JinjaGenerator {
 		// TODO Auto-generated constructor stub
 	}
 
-	protected List<String> settingsToArguments(ImplementationSettings implSettings, SoftPkg softpkg) {
-		List<String> arguments = new ArrayList<String>();
-		final IResource resource = ModelUtil.getResource(implSettings);
-		final IProject project = resource.getProject();
-		final IPath workspaceRoot = project.getWorkspace().getRoot().getLocation();
-		String spdFile = workspaceRoot.toOSString() + softpkg.eResource().getURI().toPlatformString(true);
-
-		arguments.add("--impl");
-		arguments.add(implSettings.getId());
-		arguments.add("--impldir");
-		arguments.add(implSettings.getOutputDir());
-		arguments.add("--template");
-		arguments.add(implSettings.getTemplate());
-		for (Property property : implSettings.getProperties()) {
-			arguments.add("-B"+property.getId());
-			arguments.add(property.getValue());
-		}
-		arguments.add(spdFile);
-		
-		return arguments;
-	}
-	
 	public Code getInitialCodeSettings(SoftPkg softPkg,	ImplementationSettings settings, Implementation impl) {
 		String outputDir = settings.getOutputDir();
 		if (outputDir != null && !"".equals(outputDir) && outputDir.charAt(0) == '/') {
