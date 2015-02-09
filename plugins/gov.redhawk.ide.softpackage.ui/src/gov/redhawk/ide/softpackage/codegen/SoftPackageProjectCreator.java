f74e8b3e702802aef185cf207a36b26217f2a980
Daniel Wille <daniel.wille@axiosengineering.com>
2015-02-09 14:25:35 -0500

    IDE-1096 Move to EMF model over JET

--- plugins/gov.redhawk.ide.softpackage.ui/src/gov/redhawk/ide/softpackage/codegen/SoftPackageProjectCreator.java
+++ plugins/gov.redhawk.ide.softpackage.ui/src/gov/redhawk/ide/softpackage/codegen/SoftPackageProjectCreator.java
@@ -13,40 +13,37 @@ package gov.redhawk.ide.softpackage.codegen;
 
 import gov.redhawk.ide.codegen.ImplementationSettings;
 import gov.redhawk.ide.codegen.util.ProjectCreator;
+import gov.redhawk.ide.softpackage.ui.SoftPackageUi;
 import gov.redhawk.ide.spd.IdeSpdPlugin;
 import gov.redhawk.ide.spd.generator.newcomponent.ComponentProjectCreator;
 import gov.redhawk.sca.util.SubMonitor;
 
-import java.io.ByteArrayInputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
 
+import mil.jpeojtrs.sca.spd.Author;
 import mil.jpeojtrs.sca.spd.Implementation;
+import mil.jpeojtrs.sca.spd.SoftPkg;
+import mil.jpeojtrs.sca.spd.SpdFactory;
 import mil.jpeojtrs.sca.spd.SpdPackage;
+import mil.jpeojtrs.sca.util.ScaResourceFactoryUtil;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.xmi.XMLResource;
 
 @SuppressWarnings("restriction")
 public class SoftPackageProjectCreator extends ComponentProjectCreator {
 
 	public static IFile createComponentFiles(final IProject project, final String spdName, final String spdId, final String authorName,
 		final IProgressMonitor monitor) throws CoreException {
-		final SubMonitor progress = SubMonitor.convert(monitor, "Creating SCA softpackage files", 3);
-
-		// Define the softpackage spd.xml base content, not including any implementations
-		final GeneratorArgsSoftpkg args = new GeneratorArgsSoftpkg();
-		args.setProjectName(project.getName());
-		args.setAuthorName(authorName);
-		args.setSoftPkgFile(spdName + SpdPackage.FILE_EXTENSION);
-		args.setSoftPkgId(spdId);
-		args.setSoftPkgName(spdName);
-
-		final String spdContent = new SoftPackageSpdFileTemplate().generate(args);
-		progress.worked(1);
+		final SubMonitor progress = SubMonitor.convert(monitor, "Creating SCA softpackage files", 2);
 
 		// Make sure spdFile does not already exist
 		final IFile spdFile = project.getFile(spdName + SpdPackage.FILE_EXTENSION);
@@ -54,12 +51,31 @@ public class SoftPackageProjectCreator extends ComponentProjectCreator {
 			throw new CoreException(new Status(IStatus.ERROR, IdeSpdPlugin.PLUGIN_ID, "File " + spdFile.getName() + " already exists.", null));
 		}
 
-		// Crate the softpackage spd.xml
-		try {
-			spdFile.create(new ByteArrayInputStream(spdContent.getBytes("UTF-8")), true, progress.newChild(1));
-		} catch (final UnsupportedEncodingException e) {
-			throw new CoreException(new Status(IStatus.ERROR, IdeSpdPlugin.PLUGIN_ID, "Internal Error", e));
+		// Create the model object for the .spd.xml file
+		SpdPackage.eINSTANCE.eClass();
+		final ResourceSet resourceSet = ScaResourceFactoryUtil.createResourceSet();
+		final URI spdUri = URI.createPlatformResourceURI(spdFile.getFullPath().toString(), true);
+		final XMLResource resource = (XMLResource) resourceSet.createResource(spdUri, SpdPackage.eCONTENT_TYPE);
+		final SoftPkg spd = SpdFactory.eINSTANCE.createSoftPkg();
+		resource.getContents().add(spd);
+		spd.setId(spdId);
+		spd.setName(spdName);
+		Author author = SpdFactory.eINSTANCE.createAuthor();
+		spd.getAuthor().add(author);
+		if (authorName != null) {
+			author.getName().add(authorName);
 		}
+
+		// Write the .spd.xml file to disk
+		try {
+			resource.save(null);
+			progress.worked(1);
+		} catch (IOException e) {
+			throw new CoreException(new Status(IStatus.ERROR, SoftPackageUi.PLUGIN_ID, "Unable to write SPD file", e));
+		}
+
+		// Refresh
+		spdFile.refreshLocal(IResource.DEPTH_ZERO, progress.newChild(1));
 
 		return spdFile;
 	}
