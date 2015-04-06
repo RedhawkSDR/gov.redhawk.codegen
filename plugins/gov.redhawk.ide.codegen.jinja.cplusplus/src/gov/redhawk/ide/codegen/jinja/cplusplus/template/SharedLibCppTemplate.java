affe663e77eb7ee90286c208d19431cf13584336
Daniel Wille <daniel.wille@axiosengineering.com>
2015-04-06 15:40:29 -0400

    IDE-1185 Code editor now opens for 'default' file in new C++ shared lib project

---
+++ plugins/gov.redhawk.ide.codegen.jinja.cplusplus/src/gov/redhawk/ide/codegen/jinja/cplusplus/template/SharedLibCppTemplate.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * This file is protected by Copyright. 
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under 
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package gov.redhawk.ide.codegen.jinja.cplusplus.template;
+
+import org.eclipse.core.runtime.Path;
+
+import mil.jpeojtrs.sca.spd.SoftPkg;
+import gov.redhawk.ide.codegen.ImplementationSettings;
+import gov.redhawk.ide.codegen.jinja.template.JinjaTemplate;
+
+public class SharedLibCppTemplate extends JinjaTemplate {
+
+	@Override
+    public String getDefaultFilename(SoftPkg softPkg, ImplementationSettings implSettings, String srcDir) {
+		String baseName = softPkg.getName();
+		int index = baseName.lastIndexOf('.');
+		if (index != -1) {
+			baseName = baseName.substring(index + 1);
+		}
+		return new Path(srcDir).append("src").append(baseName + ".cpp").toOSString();
+	}
+}
